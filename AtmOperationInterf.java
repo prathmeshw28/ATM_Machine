c44c6bf9f1e35224f5e1b71ff42ff56d7d1ab646
Prathmesh Watane <prathmeshwatane01@gmail.com>
2024-06-09 22:02:57 +0530

    ATM_Machine

---
+++ AtmOperationInterf.java
@@ -0,0 +1,9 @@
+package com.itshaala;
+
+public class AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+
+}
